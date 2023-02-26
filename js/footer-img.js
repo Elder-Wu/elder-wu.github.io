60c2e0f9fddc79f98cfde1a0d89d3247e8eb5834
wuming <itiswm@163.com>
2023-02-26 13:15:06 +0800

    网站最后一次更新时间YYYY-02-2023年2月26日 13:15:06

---
+++ js/footer-img.js
@@ -0,0 +1,9 @@
+(function () { 
+    const footerContainer = document.getElementById("footer")
+    if (footerContainer == null) return;
+
+    footerContainer.style.backgroundImage = "url(https://oss.noname.casa/file/nonameoss/美食街.webp)"
+    footerContainer.width = "100%"
+    footerContainer.height = "100%"
+    footerContainer.style.backgroundSize = "cover"
+})()
